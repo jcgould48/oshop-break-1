4311c47cda39e677ae7f3b68709ab0b93bd2153e
Jesse Gould <jesse.gould@codeimmersives.com>
2020-03-10 20:40:08 -0400

    fixes some bugs

--- routes/admin/adminRoutes.js
+++ routes/admin/adminRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const categoryValidation = require('./validation/categoryValidation');
 const { addProducts } = require('./products/controllers/productControllers');
+const {getAddCategory, createCategory} = require('./categories/controllers/categoryControllers')
 
 router.get('/add-category', getAddCategory);
 router.post('/add-category', categoryValidation, createCategory);
